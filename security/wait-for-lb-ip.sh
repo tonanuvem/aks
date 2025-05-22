8958ee5a558b1e7d2300ff37600a3fe806ab1e10
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-05-22 11:52:28 -0300

    Create wait-for-lb-ip.sh

---
+++ security/wait-for-lb-ip.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Nome do serviço e namespace
+SERVICE_NAME=$1
+NAMESPACE=${2:-default}  # padrão é 'default' se não for passado
+
+if [ -z "$SERVICE_NAME" ]; then
+  echo "Uso: $0 <nome-do-serviço> [namespace]"
+  exit 1
+fi
+
+echo -n "Aguardando IP externo para o serviço '$SERVICE_NAME' no namespace '$NAMESPACE'"
+
+# Espera até que o IP externo seja atribuído
+while true; do
+  EXTERNAL_IP=$(kubectl get svc "$SERVICE_NAME" -n "$NAMESPACE" -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+  
+  if [[ -n "$EXTERNAL_IP" ]]; then
+    echo -e "\n✅ IP externo disponível: $EXTERNAL_IP"
+    break
+  fi
+
+  echo -n "."
+  sleep 1
+done
