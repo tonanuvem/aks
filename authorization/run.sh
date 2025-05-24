c58779b0f11598e61d5cb104f5a71b25c6ca2152
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-05-23 22:51:00 -0300

    Update run.sh

--- authorization/run.sh
+++ authorization/run.sh
@@ -2,12 +2,14 @@
 
 # Aplique o Role:
 kubectl apply -f role-pod-read.yaml
+kubectl describe role role-pod-read
 
 #	Criação de um RoleBinding para associar o Role a um usuário (substitua <user-email> por um usuário válido no seu sistema - 
 # este é um exemplo simplificado para demonstrar RBAC dentro do cluster, a integração completa com Azure AD pode ser mais complexa e demorada):
 
 # Aplique o RoleBinding:
 kubectl apply -f pod-rolebinding-read.yaml
+kubectl describe rolebinding pod-rolebinding-read
 
 # Demonstração do uso de kubectl auth can-i:
 kubectl auth can-i list pods --as=aluno@fiap.com.br -n default
