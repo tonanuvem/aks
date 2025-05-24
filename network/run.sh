9a49d098e9ecf9b5c2d961ec207458684bdf47b8
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-05-23 23:09:56 -0300

    Update run.sh

--- network/run.sh
+++ network/run.sh
@@ -65,7 +65,7 @@ az aks delete --yes --name akstraining-rg --resource-group akstraining-rg && az 
 # Criar o cluster 2
 az group create --name akstraining-rg-2 --location westus
 az aks create --resource-group akstraining-rg-2 --name akstraining-cluster-2 --network-plugin azure --network-policy calico --node-count 2 --enable-addons monitoring --generate-ssh-keys
-az aks get-credentials --resource-group akstraining-rg-2 --name akstraining-cluster-2 --file ~/.kube/config-akstraining
+az aks get-credentials --resource-group akstraining-rg-2 --name akstraining-cluster-2 --file ~/.kube/config-akstraining  --overwrite-existing
 export KUBECONFIG=~/.kube/config-akstraining
 kubectl get nodes
 
