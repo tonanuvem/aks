c8ce1db4420b99c13d59235e203a9995f07e6bc8
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-05-23 22:43:34 -0300

    Update run.sh

--- network/run.sh
+++ network/run.sh
@@ -2,7 +2,7 @@
 
 az group create --name akstraining-rg --location eastus
 az aks create --resource-group akstraining-rg --name akstraining-cluster --node-count 2 --enable-addons monitoring --generate-ssh-keys
-az aks get-credentials --resource-group akstraining-rg --name akstraining-cluster --file ~/.kube/config-akstraining
+az aks get-credentials --resource-group akstraining-rg --name akstraining-cluster --file ~/.kube/config-akstraining --overwrite-existing
 export KUBECONFIG=~/.kube/config-akstraining
 kubectl get nodes
 
