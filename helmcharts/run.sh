899de41c9db80ed3c934706442d1d0328e933ef8
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-05-25 14:59:12 -0300

    Update run.sh

--- helmcharts/run.sh
+++ helmcharts/run.sh
@@ -10,6 +10,21 @@ helm create fiap-chart
 # Instalação do Chart no AKS:
 helm install fiap-release ./fiap-chart
 
+## SAÍDA DO COMANDO ANTERIOR ##
+## NAME: fiap-release
+# LAST DEPLOYED: Sun May 25 17:54:52 2025
+# NAMESPACE: default
+# STATUS: deployed
+# REVISION: 1
+# NOTES:
+# 1. Get the application URL by running these commands:
+#  export POD_NAME=$(kubectl get pods --namespace default -l "app.kubernetes.io/name=fiap-chart,app.kubernetes.io/instance=fiap-release" -o jsonpath="{.items[0].metadata.name}")
+#  export CONTAINER_PORT=$(kubectl get pod --namespace default $POD_NAME -o jsonpath="{.spec.containers[0].ports[0].containerPort}")
+#  echo "Visit http://127.0.0.1:8080 to use your application"
+#  kubectl --namespace default port-forward $POD_NAME 8080:$CONTAINER_PORT
+
+cp exemplo_values.yml fiap-chart/values.yaml
+
 # Upgrade do Chart:
 # Modificar o Chart (e.g., versão da imagem)
 helm upgrade fiap-release ./fiap-chart
