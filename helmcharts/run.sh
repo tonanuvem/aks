0d17f9a7c5fa62d8876fd3a8a0ef935791362cf3
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-05-25 16:48:14 -0300

    Update run.sh

--- helmcharts/run.sh
+++ helmcharts/run.sh
@@ -18,10 +18,12 @@ helm install fiap-release ./fiap-chart
 # REVISION: 1
 # NOTES:
 # 1. Get the application URL by running these commands:
-#  export POD_NAME=$(kubectl get pods --namespace default -l "app.kubernetes.io/name=fiap-chart,app.kubernetes.io/instance=fiap-release" -o jsonpath="{.items[0].metadata.name}")
-#  export CONTAINER_PORT=$(kubectl get pod --namespace default $POD_NAME -o jsonpath="{.spec.containers[0].ports[0].containerPort}")
-#  echo "Visit http://127.0.0.1:8080 to use your application"
-#  kubectl --namespace default port-forward $POD_NAME 8080:$CONTAINER_PORT
+
+IP=$(curl checkip.amazonaws.com)
+export POD_NAME=$(kubectl get pods --namespace default -l "app.kubernetes.io/name=fiap-chart,app.kubernetes.io/instance=fiap-release" -o jsonpath="{.items[0].metadata.name}")
+export CONTAINER_PORT=$(kubectl get pod --namespace default $POD_NAME -o jsonpath="{.spec.containers[0].ports[0].containerPort}")
+echo "Visit http://$IP:8080 to use your application"
+kubectl --namespace default port-forward $POD_NAME 8080:$CONTAINER_PORT
 
 cp exemplo_values.yml fiap-chart/values.yaml
 
