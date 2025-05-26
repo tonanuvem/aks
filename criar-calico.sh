046fcaed6a05c09efe159b89672b4129a771e9df
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-05-26 10:15:33 -0300

    Update criar-calico.sh

--- criar-calico.sh
+++ criar-calico.sh
@@ -1,6 +1,6 @@
 # Criar o cluster 2 com CALICO
 az group create --name akstraining-rg-calico --location westus
-az aks create --resource-group akstraining-rg-calico --name akstraining-cluster-calico --network-plugin azure --network-policy calico --node-count 2 --enable-addons monitoring --generate-ssh-keys
+az aks create --resource-group akstraining-rg-calico --name akstraining-cluster-calico --network-plugin azure --network-policy calico --node-count 2 --node-vm-size Standard_B2s --enable-addons monitoring --generate-ssh-keys
 az aks get-credentials --resource-group akstraining-rg-calico --name akstraining-cluster-calico --file ~/.kube/config-akstraining  --overwrite-existing
 export KUBECONFIG=~/.kube/config-akstraining
 kubectl get nodes
