061a2488429a5635bce39c14ac21734def8d6640
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-05-25 23:23:33 -0300

    Update app.py

--- helmcharts/python-app-env/app.py
+++ helmcharts/python-app-env/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 import os
 
 app = Flask(__name__)
@@ -9,15 +9,12 @@ def show_env_vars():
         "chave1": os.getenv("chave1", "Variável chave1 não definida"),
         "chave2": os.getenv("chave2", "Variável chave2 não definida"),
     }
-    return render_template("index.html", env_vars=env_vars)
+    return render_template("index.html", env_vars=env_vars, mostrar_todas=False)
 
-@app.route("/set_env_var", methods=["POST"])
-def set_env_var():
-    key = request.form.get("key")
-    value = request.form.get("value")
-    if key and value:
-        os.environ[key] = value
-    return redirect(url_for("show_env_vars"))
-    
+@app.route("/list_env_var")
+def list_env_var():
+    env_vars = dict(os.environ)
+    return render_template("index.html", env_vars=env_vars, mostrar_todas=True)
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
