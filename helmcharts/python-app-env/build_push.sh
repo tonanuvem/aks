59d1ec5db5c9461eef866298ca58cbca491ac640
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-05-26 10:59:35 -0300

    Update build_push.sh

--- helmcharts/python-app-env/build_push.sh
+++ helmcharts/python-app-env/build_push.sh
@@ -1,5 +1,6 @@
+echo ""
 docker login
-
+echo ""
 # Verificar o usuario que logou:
 
 USUARIO=$(jq -r '.auths."https://index.docker.io/v1/".auth' ~/.docker/config.json | base64 --decode | cut -d: -f1)
@@ -7,6 +8,8 @@ echo $USUARIO
 
 # Nome da imagem (ajuste para seu próprio repositório)
 docker build -t $USUARIO/python-env-app:latest .
+
+docker build -f Dockerfile.win -t $USUARIO/python-env-app:win .
 
 # Login no DockerHub ou outro registry (ex: Azure Container Registry)
 echo ""
