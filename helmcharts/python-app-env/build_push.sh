997b8b9b92c034e9537c7b7647f4585e37795a86
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-05-25 22:07:45 -0300

    Create build_push.sh

---
+++ helmcharts/python-app-env/build_push.sh
@@ -0,0 +1,5 @@
+# Nome da imagem (ajuste para seu próprio repositório)
+docker build -t python-env-app:latest .
+
+# Login no DockerHub ou outro registry (ex: Azure Container Registry)
+# docker push <seu-registro>/flask-env-app:latest
