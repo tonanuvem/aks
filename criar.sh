9207d3581a19b61f58dbe8d2c0ded807d350f0be
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-05-26 10:13:22 -0300

    Update criar.sh

--- criar.sh
+++ criar.sh
@@ -1,5 +1,5 @@
 az group create --name akstraining-rg --location eastus
-az aks create --resource-group akstraining-rg --name akstraining-cluster --node-count 2 --enable-addons monitoring --generate-ssh-keys
+az aks create --resource-group akstraining-rg --name akstraining-cluster --node-count 2 --node-vm-size Standard_B2s --enable-addons monitoring --generate-ssh-keys
 az aks get-credentials --resource-group akstraining-rg --name akstraining-cluster --file ~/.kube/config-akstraining --overwrite-existing
 export KUBECONFIG=~/.kube/config-akstraining
 kubectl get nodes
