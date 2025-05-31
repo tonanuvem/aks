1bb92d8868da425cc39226fbe2cabcec1d774755
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-05-31 00:37:55 -0300

    Update run.sh

--- portworx/run.sh
+++ portworx/run.sh
@@ -1,3 +1,6 @@
 # Criar Cluster com --vm-node-size Standard_B2ms (2 cpu e 16 memoria):
 
+az group create --name fiapaks-portworx --location centralus
+
 az aks create --resource-grouop fiapaks-portworx-rg --name fiapaks-portworx --node-count 2 --node-vm-size Standard_B2ms --kubernetes-version 1.31.7 --enable-addons-monitttorinig --generate-ssh-keys
+
