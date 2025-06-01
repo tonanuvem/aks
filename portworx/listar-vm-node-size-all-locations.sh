337e50d048415b94e453711cfbdc15a5ce6d037c
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-06-01 02:06:00 -0300

    Update listar-vm-node-size-all-locations.sh

--- portworx/listar-vm-node-size-all-locations.sh
+++ portworx/listar-vm-node-size-all-locations.sh
@@ -2,15 +2,19 @@
 
 CPU_COUNT=1
 MEMORY_GB=8
+OUTPUT_FILE="node-vms-list.txt"
+
+# Limpa o arquivo antes de começar
+> $OUTPUT_FILE
 
 for region in $(az account list-locations --query "[].name" -o tsv); do
-  echo "Região: $region"
+  echo "Região: $region" >> $OUTPUT_FILE
 
   az vm list-skus \
     --location $region \
     --size Standard_B* \
     --query "[?capabilities[?name=='vCPUs' && value=='$CPU_COUNT'] && capabilities[?name=='MemoryGB' && value > \`$MEMORY_GB\`]].name" \
-    -o tsv
+    -o tsv >> $OUTPUT_FILE
 
-  echo ""
+  echo "" >> $OUTPUT_FILE
 done
