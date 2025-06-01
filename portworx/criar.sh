3877dae9a71e1f4b5c60d8bb9e83479f944a9b7c
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-06-01 01:29:55 -0300

    Update criar.sh

--- portworx/criar.sh
+++ portworx/criar.sh
@@ -2,7 +2,9 @@
 
 az group create --name fiapaks-portworx --location centralus
 
-az aks create --resource-group fiapaks-portworx --name fiapaks-portworx --node-count 2 --node-vm-size Standard_B2ms --kubernetes-version 1.31.7 --enable-addons monitoring --generate-ssh-keys
+# Se precisa estritamente de 2 vCPU, mas quer mais RAM, o Standard_E2s_v3 (2 vCPU, 16 GiB RAM) é uma boa opção.
+az aks create --resource-group fiapaks-portworx --name fiapaks-portworx --node-count 2 --node-vm-size Standard_E2s_v3 --kubernetes-version 1.31.7 --enable-addons monitoring --generate-ssh-keys
+#az aks create --resource-group fiapaks-portworx --name fiapaks-portworx --node-count 2 --node-vm-size Standard_B2ms --kubernetes-version 1.31.7 --enable-addons monitoring --generate-ssh-keys
 
 az aks get-credentials --resource-group fiapaks-portworx --name fiapaks-portworx --file ~/.kube/config-fiapaks-portworx --overwrite-existing
 export KUBECONFIG=~/.kube/config-fiapaks-portworx
